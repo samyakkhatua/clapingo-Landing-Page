ef0a5f8a9a70391bedda992d321a3e06ffd5d165
Samyak Khatua <samyakkhatua@duck.com>
2023-01-09 03:10:16 +0530

    UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import Hero from "./components/Hero/Hero";
+import HowItWorks from "./components/HowItWorks/HowItWorks";
 import Navbar from "./components/Navbar/Navbar";
+import Offer from "./components/Offer/Offer";
+import Stats from "./components/Stats/Stats";
 
 function App() {
   return (
     <div className="px-32">
       <Navbar />
+      <Hero />
+      <Stats />
+      <Offer />
+      <HowItWorks />
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,4 +7,7 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  backgroundImage: {
+    'card1': "url('src/components/Offer/Screenshot 2023-01-09 at 2.09.59 AM.png)",
+  },
 }
