3880fc860f7fd0089009fc2a347a4dece24dd058
Samyak Khatua <samyakkhatua@duck.com>
2023-01-09 03:08:27 +0530

    basic components added

---
+++ src/components/Offer/Offer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function Offer() {
+  return (
+    <div className="mt-[100px] ">
+      <p className="text-4xl font-extrabold flex items-center justify-center">
+        What we offer?
+      </p>
+
+      <div className="flex mt-6 gap-4">
+        <div class="module container w-[450px] h-[450px] bg-white rounded-2xl">
+          <div class=" px-6 py-4 absolute inset-x-0 bottom-0 bg-gray-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-30">
+            <h5 class="mb-2 text-lg font-bold tracking-tight text-teal-500 ">
+              Customised Curriculum
+            </h5>
+
+            <p class="mb-3 font-normal text-white">
+              Customised sessions and module for Basic, Intermediate and
+              Advanced learners.
+            </p>
+          </div>
+        </div>
+        <div class="module container w-[450px] h-[450px] bg-white rounded-2xl">
+          <div class=" px-6 py-4 absolute inset-x-0 bottom-0 bg-gray-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-30">
+            <h5 class="mb-2 text-lg font-bold tracking-tight text-teal-500 ">
+              Customised Curriculum
+            </h5>
+
+            <p class="mb-3 font-normal text-white">
+              Customised sessions and module for Basic, Intermediate and
+              Advanced learners.
+            </p>
+          </div>
+        </div>
+        <div class="module container w-[450px] h-[450px] bg-white rounded-2xl">
+          <div class=" px-6 py-4 absolute inset-x-0 bottom-0 bg-gray-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-30">
+            <h5 class="mb-2 text-lg font-bold tracking-tight text-teal-500 ">
+              Customised Curriculum
+            </h5>
+
+            <p class="mb-3 font-normal text-white">
+              Customised sessions and module for Basic, Intermediate and
+              Advanced learners.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
