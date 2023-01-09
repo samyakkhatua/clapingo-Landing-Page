23597dfe121b5d2fc9dd63d2c84f606d94f744af
Samyak Khatua <samyakkhatua@duck.com>
2023-01-09 16:29:35 +0530

    loggedin page updated

--- src/pages/LoggedIn.jsx
+++ src/pages/LoggedIn.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function LoggedIn() {
   return (
-    <div className='flex justify-center mt-96 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>You are logged In!</div>
+    <div className='flex flex-col justify-center items-center mt-96'>
+        <div className=' mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>You are logged In!</div>
+        <p className='mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48'>You are a user.</p>
+    </div>
   )
 }
